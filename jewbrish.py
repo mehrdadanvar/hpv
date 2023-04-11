a5c2e697ab670053ac9aeb78acbe8f5f5fd19d7a
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-04-11 13:05:04 -0700

    added some trash code

--- jewbrish.py
+++ jewbrish.py
@@ -12,3 +12,21 @@ def recode_values(df):
         else:
             print(False)
     return df, translation_list
+
+import matplotlib.pyplot as plt
+sample =1
+know = 1
+know.hist("k_finalscore",bins=20)
+plt.show()
+grouped = sample.groupby("d_gender")
+fig, ax = plt.subplots()
+for name, group in grouped:
+    ax.hist(group['k_finalscore'], bins=5, alpha=0.5, label=name)
+
+# add labels and legend to the plot
+ax.set_xlabel('Final Score')
+ax.set_ylabel('Count')
+ax.legend()
+
+# show the plot
+plt.show()
